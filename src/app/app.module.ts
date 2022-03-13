88756e3cf8afa0bef38fb3bc7fe2eec3f0c20051
Javier Moreno Rodríguez <72435195+Javiemr@users.noreply.github.com>
2022-03-13 21:03:30 +0100

    Ultimo commit
    Adduser

---
+++ src/app/app.module.ts
@@ -0,0 +1,67 @@
+import {ButtonModule} from 'primeng/button';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { TableModule } from 'primeng/table';
+import { AppRoutingModule } from './app-routing.module';
+import {CheckboxModule} from 'primeng/checkbox';
+import { AppComponent } from './app.component';
+import { UsuariosComponent } from './components/usuarios/usuarios.component';
+import { GruposComponent } from './components/grupos/grupos.component';
+import {ConfirmationPopoverModule} from 'angular-confirmation-popover';
+import {DialogModule} from 'primeng/dialog';
+import { MessagesModule } from 'primeng/messages';
+import {ToastModule} from 'primeng/toast';
+import {TabViewModule} from 'primeng/tabview';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {ConfirmDialogModule} from 'primeng/confirmdialog';
+import {CardModule} from 'primeng/card';
+import {InputTextModule} from 'primeng/inputtext';
+import {PickListModule} from 'primeng/picklist';
+import { IndexComponent } from './index/index.component';
+import {MessageModule} from 'primeng/message';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideStorage,getStorage } from '@angular/fire/storage';
+import { FormsModule } from '@angular/forms';
+
+
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UsuariosComponent,
+    GruposComponent,
+    IndexComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ButtonModule,
+    TableModule,
+    CheckboxModule,
+    ConfirmationPopoverModule,
+    DialogModule,
+    MessagesModule,
+    FormsModule,
+    ToastModule,
+    TabViewModule,
+    BrowserAnimationsModule,
+    ConfirmDialogModule,
+    CardModule,
+    InputTextModule,
+    PickListModule,
+    MessageModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
+    
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
